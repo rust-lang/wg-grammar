5e6385d3c65cdad666656fb144375a1c963253e9
cad97 <cad97@cad97.com>
2019-05-13 17:42:38 -0400

    Specify parse module warning exemptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-#[allow(warnings)]
+#[allow(
+    unused_imports,
+    non_camel_case_types,
+    clippy::redundant_closure_call,
+    clippy::type_complexity,
+    clippy::cognitive_complexity,
+    clippy::large_enum_variant
+)]
 pub mod parse {
     include!(concat!(env!("OUT_DIR"), "/parse.rs"));
 }
